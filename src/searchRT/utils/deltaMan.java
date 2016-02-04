527ac0a907ff661812dc5580901430646c4b333a
flinn2112 <flinn2112@yahoo.com>
2016-02-04 18:47:33 +0100

    Test

--- src/searchRT/utils/deltaMan.java
+++ src/searchRT/utils/deltaMan.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package searchRT.utils;
+package searchRT.utils; 
 
 import java.io.* ;
 import java.util.Hashtable ;
